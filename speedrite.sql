dee269e5a1f49baad22040f050e23f754556ccb7
polarathene <polarathene@users.noreply.github.com>
2015-06-22 04:37:34 +1200

    Updated phone field Switched `phone` from bigint(15) to varchar(50), more than enough room to handle RFC3966 extensions and preprend `+` for clarity.

--- speedrite.sql
+++ speedrite.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50543
 File Encoding         : 65001
 
-Date: 2015-06-21 18:36:41
+Date: 2015-06-22 04:31:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,7 +24,7 @@ CREATE TABLE `main` (
   `entry_code` varchar(20) NOT NULL COMMENT 'Length and format not clear, assuming string',
   `name` varchar(255) NOT NULL COMMENT 'Single field for customers name, due to brief separate table is not required',
   `email` varchar(254) NOT NULL COMMENT 'Email length of 254 as per RFC3696 Errata ID 1690: http://www.rfc-editor.org/errata_search.php?rfc=3696',
-  `phone` bigint(15) NOT NULL COMMENT 'Based on E.164: https://en.wikipedia.org/?title=E.164 ,extensions not supported',
+  `phone` varchar(50) NOT NULL COMMENT 'Store values in the E.164 format: https://en.wikipedia.org/?title=E.164 ,handle extensions via RFC3966 which appends ";ext=1234"',
   `store` varchar(255) NOT NULL COMMENT 'Not clear if specific stores(FK INT) or variety(VARCHAR)',
   `model_purchased` int(11) unsigned NOT NULL COMMENT 'References model table id field',
   `serial_number` varchar(255) NOT NULL COMMENT 'Length and format not clear, assuming string',
